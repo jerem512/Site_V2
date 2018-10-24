7671b4f4723b66158aa7a5f30a3097899bd13d3d
root <jeremymarchal90@gmail.com>
2018-10-24 18:16:57 +0200

    commit 5

--- cible_mdp.php
+++ cible_mdp.php
@@ -6,7 +6,7 @@
 	$pass2 = $_POST['new_pass2'];
 		if (empty($pass || $pass2)){
 			echo 'Les champs sont vides';
-			header("Refresh:0;URL=moncompte.php");
+			header("Refresh:0;URL=compte.php");
 	}
 		elseif ($pass == $pass2) {
 			$hashedPassword = password_hash($pass, PASSWORD_ARGON2I);
@@ -14,10 +14,10 @@
 			$req->bindParam(':id', $id);
 		$req->bindParam(':password', $hashedPassword);
 		echo 'Mot de passe changé avec succès';
-		header("Refresh:0;URL=moncompte.php");
+		header("Refresh:0;URL=compte.php");
 	}
 		else {
 			echo 'Les mots de passe sont différents';
-			header("Refresh:0;URL=moncompte.php");
+			header("Refresh:0;URL=compte.php");
 	}
 ?>
--- forget.php
+++ forget.php
@@ -1,2 +1,10 @@
-T'AS OUBLIÉ TON MDP ????
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mot de passe oublié</title>
+</head>
+<body>
+	
+</body>
+</html>
